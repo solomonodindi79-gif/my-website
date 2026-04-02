0df25d0803bd10671da33118f1d4480006a5fba5
solomonodindi79-gif <solomonodindi79@gmail.com>
2026-04-02 14:08:58 +0300

    Add files via upload

--- script.js
+++
@@ -1,9 +0,0 @@
-// Greeting button
-function greetSolomon() {
-  alert("Hello Solomon! Welcome to my website.");
-}
-
-// Dark mode toggle
-function toggleDarkMode() {
-  document.body.classList.toggle("dark-mode");
-}
